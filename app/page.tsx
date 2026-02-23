856cc7f1b09b66c59fdb78c80072d8bd5e0f016d
EhsanTgv <EhsanTgv@gmail.com>
2026-02-23 07:57:59 +0300

    Routing and Navigation

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,11 @@
 import Image from 'next/image'
+import Link from 'next/link'
 
 export default function Home() {
   return (
-    <main><h1>Welcome to the Home Page</h1></main>
-  ) 
+    <main>
+      <h1>Welcome to the Home Page</h1>
+      <Link href="/users">Go to Users Page</Link>
+    </main>
+  )
 }
