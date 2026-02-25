c775ba7151535036674f75147565b912a96ee17c
EhsanTgv <EhsanTgv@gmail.com>
2026-02-25 09:05:43 +0300

    Client and Server components

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,13 @@
 import Image from 'next/image'
 import Link from 'next/link'
+import ProductCard from './components/ProductCard'
 
 export default function Home() {
   return (
     <main>
       <h1>Welcome to the Home Page</h1>
       <Link href="/users">Go to Users Page</Link>
+      <ProductCard />
     </main>
   )
 }
